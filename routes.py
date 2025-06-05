60ac75f5c4bdd4c37421395de3e3d05373dec7d0
kongkitsada2 <145382167+kongkitsada2@users.noreply.github.com>
2025-06-05 13:12:25 +0700

    Update routes.py

--- routes.py
+++ routes.py
@@ -3,6 +3,7 @@ from logic.submit import submit_qr_logic
 from logic.check_deduct import check_and_deduct_logic
 from logic.get_sheet_data import get_sheet_data
 from logic.chatAI import chat_ai_logic
+from logic.last_count import read_last_response_counts
 
 def register_routes(app):
     @app.route('/')
@@ -24,3 +25,7 @@ def register_routes(app):
     @app.route('/chat_ai', methods=['POST'])
     def chat_ai():
         return chat_ai_logic(request)
+        
+    @app.route('/show_last_counts')
+    def show_last_counts():
+        return jsonify(read_last_response_counts())
