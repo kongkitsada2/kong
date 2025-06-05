e043e3ea16fe5217b102dd23b5bcefd66a85e342
kongkitsada2 <145382167+kongkitsada2@users.noreply.github.com>
2025-06-05 12:59:10 +0700

    Add files via upload

---
+++ last_count.py
@@ -0,0 +1,19 @@
+import os
+from flask import jsonify
+
+def read_last_response_counts():
+    folder_path = ""
+    filenames = {
+        "last_response_count_ตอบกลับลูกสูบ.txt": "ปั๊มลมลูกสูบ",
+        "last_response_count_ตอบกลับไร้น้ำมัน.txt": "ปั๊มลมไร้น้ำมัน",
+        "last_response_count_ตอบกลับโรตารี่.txt": "ปั๊มลมโรตารี่"
+    }
+    results = {}
+    for filename, display_name in filenames.items():
+        path = os.path.join(folder_path, filename)
+        if os.path.exists(path):
+            with open(path, "r", encoding="utf-8") as f:
+                results[display_name] = f.read().strip()
+        else:
+            results[display_name] = "ไม่พบไฟล์"
+    return results
