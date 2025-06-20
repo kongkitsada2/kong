d7d76f094e686d18437d403c292426d330bb4bb1
kongkitsada2 <145382167+kongkitsada2@users.noreply.github.com>
2025-06-20 14:48:56 +0700

    Update last_count.py

--- logic/last_count.py
+++ logic/last_count.py
@@ -2,14 +2,12 @@ import os
 from flask import jsonify
 
 def read_last_response_counts():
-    folder_path = os.path.join(os.path.dirname(__file__), "../static")  # หรือ "static" ถ้าอยู่ที่เดียวกัน
-
+    folder_path = ""
     filenames = {
         "last_response_count_ตอบกลับลูกสูบ.txt": "ปั๊มลมลูกสูบ",
         "last_response_count_ตอบกลับไร้น้ำมัน.txt": "ปั๊มลมไร้น้ำมัน",
         "last_response_count_ตอบกลับโรตารี่.txt": "ปั๊มลมโรตารี่"
     }
-
     results = {}
     for filename, display_name in filenames.items():
         path = os.path.join(folder_path, filename)
