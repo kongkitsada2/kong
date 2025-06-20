ca0ccb017480130bc8652b54ddffe51f376a652f
kongkitsada2 <145382167+kongkitsada2@users.noreply.github.com>
2025-06-20 13:35:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ from routes import register_routes
 app = Flask(__name__)
 register_routes(app)
 
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('.', filename)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 '''
