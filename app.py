007f062ae3bc5ac1442dfeafd0b514e8563bcef2
kongkitsada2 <145382167+kongkitsada2@users.noreply.github.com>
2025-06-20 13:37:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ register_routes(app)
 
 @app.route('/static/<path:filename>')
 def serve_static(filename):
-    return send_from_directory('.', filename)
+    return send_from_directory('static', filename)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
